f14d26526f32832a057a54568638f7a0fb92a244
AlexDissaux <dissauxalex@gmail.com>
2019-12-10 10:05:35 +0100

    option features implements

--- angularProject/src/app/admin/edit/edit.component.ts
+++ angularProject/src/app/admin/edit/edit.component.ts
@@ -15,25 +15,17 @@ export class EditComponent implements OnInit {
 
   action: string;
   options: string[];
-  checkoutForm; // Stock form value form Stock builder
-
-  constructor(
-    private route: ActivatedRoute,
-    private alumnusService: AlumnusService,
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private actionPerformed: ActionPerformedService,
-    private dataOptionService: DataOptionService
-  ) {
-  }
+  checkoutForm;
+  idAlumnus: number;
 
   ngOnInit() {
-
     this.options = this.dataOptionService.getOptions();
     this.action = this.actionPerformed.getAction();
 
     if (this.action === 'Modify') {
+
       const alumnusToModify: Alumnus = this.alumnusService.findOne(this.actionPerformed.getAlumnusId());
+      this.idAlumnus = alumnusToModify.id;
 
       this.checkoutForm = this.formBuilder.group({
         id: alumnusToModify.id,
@@ -45,8 +37,11 @@ export class EditComponent implements OnInit {
         salaire: alumnusToModify.salaire
       });
     } else {
+
+      this.idAlumnus = this.alumnusService.generateId();
+
       this.checkoutForm = this.formBuilder.group({
-        id: this.alumnusService.generateId(),
+        id: this.idAlumnus,
         name: '',
         promotion: '',
         option: '',
@@ -55,11 +50,25 @@ export class EditComponent implements OnInit {
         salaire: ''
       });
     }
+  }
 
+  constructor(
+    private route: ActivatedRoute,
+    private alumnusService: AlumnusService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private actionPerformed: ActionPerformedService,
+    private dataOptionService: DataOptionService
+  ) {
   }
 
 
   onclickSubmit(formData) {
+
+    // Add Alumnus Id to data
+    formData.id = this.idAlumnus;
+
+    // Test if it is Modify or Add Mode
     if (this.action === 'Modify') {
       this.alumnusService.modify(formData);
       this.router.navigate(['']);
